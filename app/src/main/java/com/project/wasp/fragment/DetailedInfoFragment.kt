f92c11a90c1104d07e2ffa0029802d7506b206e0
Shekhar Suman <62455266+Shekhar0050M@users.noreply.github.com>
2024-06-13 14:59:19 +0530

    Navigation Function - Init config

---
+++ app/src/main/java/com/project/wasp/fragment/DetailedInfoFragment.kt
@@ -0,0 +1,24 @@
+package com.project.wasp.fragment
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.project.wasp.R
+
+class DetailedInfoFragment: Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.detailed_information, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        // You can initialize UI components and set listeners here
+    }
+}
---
+++ app/src/main/java/com/project/wasp/fragment/GraphicalInfoFragment.kt
@@ -0,0 +1,24 @@
+package com.project.wasp.fragment
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.project.wasp.R
+
+class GraphicalInfoFragment: Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.graphical_information, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        // You can initialize UI components and set listeners here
+    }
+}
