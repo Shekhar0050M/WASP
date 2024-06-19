1895c3f8cadcb4974189bb50c928c7405df2c89a
Shekhar Suman <62455266+Shekhar0050M@users.noreply.github.com>
2024-06-19 14:49:26 +0530

    Tab change Dynamic audio amplitude value present Success Refactor

--- app/src/main/java/com/project/wasp/MainActivity.kt
+++ app/src/main/java/com/project/wasp/MainActivity.kt
@@ -1,23 +1,14 @@
 package com.project.wasp
 
 import android.os.Bundle
-import android.os.Handler
-import android.os.Looper
-import android.util.Log
-import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
 import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.bottomnavigation.BottomNavigationView
-import com.project.wasp.ioutils.AudioUtils
-import com.project.wasp.utils.SharedPreferencesManager
 
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var audioRecorder: AudioUtils
-    private lateinit var amplitudeTextView: TextView
-    private lateinit var sharedPreferencesManager: SharedPreferencesManager
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -25,42 +16,8 @@ class MainActivity : AppCompatActivity() {
         val bottomNavigationView: BottomNavigationView = findViewById(R.id.bottom_navigation)
         val navController = findNavController(R.id.nav_host_fragment)
         bottomNavigationView.setupWithNavController(navController)
-
-        // Initialize SharedPreferencesManager
-        sharedPreferencesManager = SharedPreferencesManager(this)
-
-        // Initialize AudioUtils and TextView
-        audioRecorder = AudioUtils(this)
-        amplitudeTextView = findViewById(R.id.audioUtils)
-
-        // Start recording
-        audioRecorder.startRecording()
-//
-//        // Handler to update amplitudeTextView and save amplitudeString in SharedPreferences
-        val handler = Handler(Looper.getMainLooper())
-        handler.post(object : Runnable {
-            override fun run() {
-                val amplitude = audioRecorder.calculateAmplitude()
-                val amplitudeString = buildString {
-                    append(getString(R.string.audio_amplitude_text))
-                    append(": ")
-                    append(amplitude)
-                }
-//
-//                // Update amplitudeTextView
-//                amplitudeTextView.text = sharedPreferencesManager.getValue("amplitudeText",
-//                    getString(R.string.audiotechnicaltext))
-                Log.d("SharedPreferencesManager", "Saving value: $amplitudeString with key: amplitudeText")
-//                // Save amplitudeString in SharedPreferences
-                sharedPreferencesManager.saveValue("amplitudeText", amplitudeString)
-//
-//                // Schedule the next update
-                handler.postDelayed(this, 1000) // Update every second (1000 milliseconds)
-            }
-        })
     }
     override fun onDestroy() {
         super.onDestroy()
-        audioRecorder.stopRecording() // Stop recording when MainActivity is destroyed
     }
 }
