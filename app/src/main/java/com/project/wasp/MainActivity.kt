0ce7fffcf8ecc0c3511a32a17d93edf323b4d4a5
Shekhar Suman <62455266+Shekhar0050M@users.noreply.github.com>
2024-06-26 16:29:21 +0530

    Basic theme code implemented

--- app/src/main/java/com/project/wasp/MainActivity.kt
+++ app/src/main/java/com/project/wasp/MainActivity.kt
@@ -2,24 +2,32 @@ package com.project.wasp
 
 import android.Manifest
 import android.annotation.SuppressLint
+import android.content.BroadcastReceiver
+import android.content.Context
 import android.content.Intent
+import android.content.IntentFilter
 import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
 import androidx.navigation.findNavController
 import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.project.wasp.fragment.DetailedInfoFragment
 import com.project.wasp.systemutils.AppForegroundService
 import com.project.wasp.systemutils.PermissionsHelper
+import java.util.Calendar
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var permissionsHelper: PermissionsHelper
+    private lateinit var timeReceiver: BroadcastReceiver
 
     @SuppressLint("InlinedApi")
     override fun onCreate(savedInstanceState: Bundle?) {
         Log.d("MainActivity", "onCreate called")
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+        setThemeBasedOnTime()
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
@@ -38,6 +46,16 @@ class MainActivity : AppCompatActivity() {
         val bottomNavigationView: BottomNavigationView = findViewById(R.id.bottom_navigation)
         val navController = findNavController(R.id.nav_host_fragment)
         bottomNavigationView.setupWithNavController(navController)
+
+        // Theme Implementation
+        timeReceiver = object : BroadcastReceiver() {
+            override fun onReceive(context: Context, intent: Intent) {
+                setThemeBasedOnTime()
+                recreate() // Restart activity to apply new theme
+            }
+        }
+
+        registerReceiver(timeReceiver, IntentFilter(Intent.ACTION_TIME_TICK))
 
         // Activate the Detailed Info fragment
         Log.d("MainActivity","StartedDetailedInfoFragment")
@@ -62,7 +80,26 @@ class MainActivity : AppCompatActivity() {
     }
 
 
+    private fun setThemeBasedOnTime() {
+        val calendar = Calendar.getInstance()
+        val hour = calendar.get(Calendar.HOUR_OF_DAY)
+
+        when (hour) {
+            in 6..8 -> setTheme(R.style.Theme_Morning)
+            in 9..11 -> setTheme(R.style.Theme_LateMorning)
+//            in 12..14 -> setTheme(R.style.Theme_Afternoon)
+//            in 15..17 -> setTheme(R.style.Theme_LateAfternoon)
+//            in 18..20 -> setTheme(R.style.Theme_Evening)
+//            in 21..23 -> setTheme(R.style.Theme_EarlyNight)
+//            in 0..0 -> setTheme(R.style.Theme_Midnight)
+//            in 0..2 -> setTheme(R.style.Theme_Night)
+//            else -> setTheme(R.style.Theme_LateNight)
+            else -> setTheme(R.style.Theme_Night)
+        }
+    }
+
     override fun onDestroy() {
         super.onDestroy()
+        unregisterReceiver(timeReceiver)
     }
 }
